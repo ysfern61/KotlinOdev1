31006da18c0c7fec8f8fa495e506c28b71063463
ysfern61 <yusuferencihan13@gmail.com>
2025-04-27 16:04:56 +0300

    odev1

---
+++ odev1/app/src/main/java/com/example/odev1/odev1.kt
@@ -0,0 +1,104 @@
+package com.example.odev1
+
+fun main() {
+    val sehir = "İstanbul"
+    val ulke = "Türkiye"
+    val telefon = "+90 555 555 55 55"
+    val postaKodu = "34000"
+    val email = "ornek@gmail.com"
+    val meslek = "Yazılım Mühendisi"
+    val stokMiktari = 150
+    val musteriAdi = "Eren Yılmaz"
+    val bakiye = 2500.75
+    val dogumGunu = "1998-05-14"
+    val maas = 15000
+    val medeniDurum = "Bekar"
+    val urunYorum = "Ürün çok kaliteli."
+
+    val odemeTarihi = "2025-05-01"
+    val odeme = 499.99
+    val siparisAdeti = 3
+    val arabaModeli = "Toyota Corolla"
+    val kitapAdi = "Kotlin Programlama"
+    val yayinlamaTarihi = "2020-09-15"
+    val indirimMiktari = 50
+    val odaSayisi = 2
+    val enlem = 39.9111
+    val boylam = 41.2744
+    val urunAdi = "Kablosuz Kulaklık"
+    val yemekFiyati = 120.5
+
+    val marka = "Samsung"
+    val muzikAdi = "Bohemian Rhapsody"
+    val videoSuresi = "5 dakika"
+    val urunPuani = 4.8
+    val resimAdi = "dogal_manzara.jpg"
+    val dosyaFormati = "jpg"
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "iPhone 15"
+    val ekranBoyutu = "6.1 inç"
+    val agirlik = 180.0
+
+    val ulusalGun = "29 Ekim Cumhuriyet Bayramı"
+    val tatilGunu = "1 Mayıs Emek ve Dayanışma Günü"
+    val rezervasyonTarihi = "2025-06-10"
+    val sokakAdi = "Bağdat Caddesi"
+    val otobusHatti = "34AS"
+    val kalanDakika = 12
+    val takipKodu = "TR123456789"
+    val kuponSuresi = "30 gün"
+    val kuponKodu = "INDIRIM50"
+    val faturaAdresi = "Atatürk Mah. No:123 İstanbul"
+
+    // Çıktılar
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorum: $urunYorum")
+
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ağırlık: $agirlik")
+
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+}
